41be7cfb6d4fd5501cfbf784d6396b50c8c6c82c
Lazar Jovanovic <lazar@summations.com>
2023-03-07 10:24:21 -0800

    Update world time dashboard script

--- commands/dashboard/world-time.sh
+++ commands/dashboard/world-time.sh
@@ -12,13 +12,24 @@
 #
 # Documentation:
 # @raycast.description Show the time from elsewhere in the world
-# @raycast.author Jesse Claven
-# @raycast.authorURL https://github.com/jesse-c
+# @raycast.author Lazar Jovanovic
+# @raycast.authorURL https://github.com/lazarjov
+
+# comment: Script modified from the one written by Jesse Claven (https://github.com/jesse-c)
 
 # Timezones can be found in /usr/share/zoneinfo
 
-nyc=$(TZ=America/New_York date +"%H:%M")
-lon=$(TZ=Europe/London date +"%H:%M")
-bne=$(TZ=Australia/Brisbane date +"%H:%M")
+# Set the time format to 12- or 24-hour
+FORMAT="12" # 12 or 24
 
-echo "New York City: $nyc | London: $lon | Brisbane: $bne"
+if [ "$FORMAT" = "12" ]; then
+    export TIMEFORMAT="%-I:%M %p"
+else
+    export TIMEFORMAT="%-H:%M"
+fi
+
+YVR=$(TZ=America/Vancouver date +"$TIMEFORMAT")
+YYZ=$(TZ=America/Toronto date +"$TIMEFORMAT")
+BEG=$(TZ=Europe/Belgrade date +"$TIMEFORMAT")
+
+echo "Vancouver: $YVR | Toronto: $YYZ | Belgrade: $BEG"
