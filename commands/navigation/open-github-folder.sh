ecfcc323c2e111d1019a13594746f7c2ad928a88
Lazar Jovanovic <26031133+lazarjov@users.noreply.github.com>
2023-07-16 16:36:49 -0700

    add open github folder script

---
+++ commands/navigation/open-github-folder.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open GitHub Folder
+# @raycast.mode silent
+# @raycast.packageName Navigation
+
+# Optional parameters:
+# @raycast.icon images/folder-github.png
+
+# Documentation:
+# @raycast.description Opens the GitHub folder in the Finder.
+
+open ~/GitHub
